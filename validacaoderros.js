944c903ab767882939e75378dc95ea2c39b0c07e
Stefanie Cristinie <ste.cristinie@hotmail.com>
2022-08-11 18:58:46 -0300

    Debugging e Error Handling js

---
+++ validacaoderros.js
@@ -0,0 +1,33 @@
+function validacaoDeErros(arr, num) {
+	try {
+		if (!arr && !num) throw new ReferenceError('Envie os parâmetros');
+
+		if (typeof arr !== 'object') throw new TypeError('O array precisa ser do tipo object');
+
+		if (typeof num !== 'number') throw new TypeError('O num precisa ser do tipo number');
+
+		if (arr.length !== num) throw new RangeError('Tamanho inválido!');
+
+		return arr;
+	} 
+    catch (e) {//e = erro
+		if (e instanceof ReferenceError) {
+			console.log('Este erro é um ReferenceError!');
+			console.log(e.message);
+		} else if (e instanceof TypeError) {
+			console.log('Este erro é um TypeError!');
+			console.log(e.message);
+		} else if (e instanceof RangeError) {
+			console.log('Este erro é um RangeError!');
+			console.log(e.message);
+		} else {
+			console.log('Tipo de erro não esperado:' + e);
+		}
+	}
+}
+
+//console.log(validacaoDeErros());
+//console.log(validacaoDeErros(5, 5));
+//console.log(validacaoDeErros([], 'a'));
+//console.log(validacaoDeErros([], 5));
+console.log(validacaoDeErros([1, 2, 3, 4, 5], 5));
